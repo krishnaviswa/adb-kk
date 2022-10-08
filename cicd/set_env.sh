4636f5529f88476c7d8295fa19295824781840f0
KKumarData <61173325+KKumarData@users.noreply.github.com>
2022-10-08 00:13:29 -0500

    changes to set_env.sh

--- cicd/set_env.sh
+++ cicd/set_env.sh
@@ -6,8 +6,6 @@ sudo yum install -y jq
 # set region for boto3
 aws configure set region "us-east-1"
 
-python3 -m pip install boto3 ec2-metadata gitpython apache-airflow
-
 # Clone repo and install some useful python packages
 git clone https://github.com/krishnaviswa/adb-kk.git
 cd adb-kk
